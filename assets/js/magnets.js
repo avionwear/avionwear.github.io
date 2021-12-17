3f2df2384eb6c7d09462eb8187132b7684adf3d1
gopalakrishnan-r <rgopalakrishnanmca@gmail.com>
2021-12-17 19:34:56 +0530

    AvionWear -> Magnet reverse order functionality added

--- assets/js/magnets.js
+++ assets/js/magnets.js
@@ -1,6 +1,7 @@
 /*
     @author R.Gopalakrishnan
     Product json Developed on 26/10/2021.
+    Reverse order in Json updated on 17/12/2021.
 */
 
 $(document).ready(function() {
@@ -9,38 +10,41 @@ $(document).ready(function() {
         function(data) {
 
             var indexProductList = ''
-            let tornnado = ''
+            var descendingOrderData = Object.keys(data)
 
-            $.each(data, function(key, value) {
+
+            for (var i = descendingOrderData.length - 1; i >= 0; i--) {
 
                 indexProductList += '<div class="col-lg-4 col-md-6 text-center mt-2">'
 
                 indexProductList += '<div class="card" style="width: 20rem;">'
-                indexProductList += '<a href="' + value.url + '" >'
+                indexProductList += '<a href="' + data[descendingOrderData[i]].url + '" >'
                 indexProductList += '<img class="card-img-top" src="'
-                indexProductList += value.imageUrl
+                indexProductList += data[descendingOrderData[i]].imageUrl
                 indexProductList += '" alt="Card image cap">'
                 indexProductList += '</a>'
 
                 indexProductList += '<div class="card-body">'
                 indexProductList += '<h5>'
-                indexProductList += '<a href="' + value.url + '" >'
-                indexProductList += key
+                indexProductList += '<a href="' + data[descendingOrderData[i]].url + '" >'
+                indexProductList += data[descendingOrderData[i]].name
                 indexProductList += '</a>'
                 indexProductList += '</h5>'
                 indexProductList += '<p class="card-text">M.R.P : ₹ <span class=text-decoration text-muted>'
-                indexProductList += value.mrp
+                indexProductList += data[descendingOrderData[i]].mrp
                 indexProductList += '</span><strong> '
-                indexProductList += value.price
+                indexProductList += data[descendingOrderData[i]].price
                 indexProductList += '</strong></p>'
-                indexProductList += '<a href="' + value.buy + '" target="_blank" rel="noopener"><button>Buy</button></a>'
+                indexProductList += '<a href="' + data[descendingOrderData[i]].buy + '" target="_blank" rel="noopener"><button>Buy</button></a>'
                 indexProductList += '</div>'
 
                 indexProductList += '</div>'
 
                 indexProductList += '</div>'
 
-            });
+
+
+            }
 
 
             $('#products-list').append(indexProductList);
